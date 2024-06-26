f880663b6f5d30aa43af7030e86c09aeae658938
Awesoddin <144103363+Awesoddin@users.noreply.github.com>
2024-06-26 23:10:02 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-console.log("hi");
-let cross = document.querySelectorAll('.cross');
-
-cross.forEach(crosses => {
-    crosses.addEventListener("click", function() {
-        alert("Feature will come soon!")
-    });
-});
